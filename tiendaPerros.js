86bb9b374bfd7064594f73b3ff200122d0bb6be7
Ine2024 <168091558+Ine2024@users.noreply.github.com>
2024-05-29 08:56:07 -0300

    agrego api

--- tiendaPerros.js
+++ tiendaPerros.js
@@ -1,19 +1,23 @@
-let perrito = document.querySelector("#dogContainer");
-let perritoCopia = perrito.cloneNode(true);
+document.addEventListener('DOMContentLoaded', function() {
+    let perrito = document.querySelector("#dogContainer");
+    let perritoCopia = perrito.cloneNode(true);
 
-let contenedor = document.querySelector("article");
-perrito.remove()
+    let contenedor = document.querySelector(".fotos_clientes");
+    perrito.remove();
 
-    fetch("https://dog.ceo/api/breeds/list/all")
-        .then( response => response.json())
-        .then(data => { 
-            //procesamiento de la info que llega de la API
-            
-            let nuevoPerrito = perritoCopia.cloneNode(true);
-            nuevoPerrito.querySelector("#imagen_perro").src = data.results[0].message;
-            nuevoPerrito.querySelector("#imagen_perro").alt = "foto de perrito";
-            contenedor.appendChild(nuevoPerrito);
-            
-            console.log(data.results[0]);
-        })
-    .catch (error => console.log("Ocurrió un error al mostrar al perro"));
+    const apiKey = 'live_jGjcBrmnaQl3JwUKUQQWJZRE6d70tgwGkaQn7aoRZwdMStREMML47FGGtVcvTqrT'; 
+
+    fetch('https://api.thedogapi.com/v1/images/search', {
+        headers: {
+            'x-api-key': apiKey
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        let nuevoPerrito = perritoCopia.cloneNode(true);
+        nuevoPerrito.querySelector("#imagen_perro").src = data[0].url;
+        nuevoPerrito.querySelector("#imagen_perro").alt = "foto de perrito";
+        contenedor.appendChild(nuevoPerrito);
+    })
+    .catch(error => console.log("Ocurrió un error al mostrar al perro", error));
+});
